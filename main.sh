360ffa9653143bf308619e7e71fc839f73cf4245
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-08 02:35:16 +0530

    Initial commit

---
+++ main.sh
@@ -0,0 +1,14 @@
+// delete branch locally
+git branch -d localBranchName
+
+// delete branch remotely
+git push origin --delete remoteBranchName
+
+// delete branch locally
+git branch -d localBranchName
+
+// delete branch remotely
+git push origin --delete remoteBranchName
+
+// refresh branch list
+git fetch -p
